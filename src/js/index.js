c07b528870ebf92b4b10a9a634ff6b3dcb0999f6
nihilncunia <nihil_ncunia@naver.com>
2022-05-23 01:12:49 +0900

    2022 0523 vscode 최적화 + 템플릿 업데이트

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-const $divHello = document.getElementById('hello');
+var $divHello = document.getElementById('hello');
 $divHello.textContent = 'Hello World!!';
 console.log('준비 완료!');
-//# sourceMappingURL=index.js.map
