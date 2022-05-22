bb99190f46f9d0327657402634c1bac6da42065a
nihilncunia <nihil_ncunia@naver.com>
2022-05-22 16:51:59 +0900

    2022 0522 템플릿 업데이트

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,4 @@
 const $divHello = document.getElementById('hello');
-$divHello.textContent = 'Hello World!!';
+$divHello.textContent = 'Hello World!!!';
 
-console.log($divHello);
+console.log('준비 완료.');
